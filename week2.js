4c07d24aaa1a972d8f23866539c8d885b8cea1dc
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-15 16:32:32 +0100

    adds the perimeter function

--- week2.js
+++ week2.js
@@ -4,8 +4,11 @@ function displayDayTime() {
 }
 
 function areaPerimeter(num1, num2, num3) {
-
+    var perimeter = num1 + num2 + num3;
+    console.log("the perimeter is " + perimeter );
+    var area = ( 1/ 2) * num1 * num2;
+    console.log("the area is " + area);
 }
 
 displayDayTime();
-areaPerimeter(1, 2, 3);
+areaPerimeter(10, 2, 3);
