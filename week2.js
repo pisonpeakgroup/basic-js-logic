10d9cc5698189c5fe2f36a164661b10e39825de0
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-15 16:19:02 +0100

    adds datetime function

---
+++ week2.js
@@ -0,0 +1,11 @@
+function displayDayTime() {
+    var currentDate = new Date();
+    console.log(currentDate);
+}
+
+function areaPerimeter(num1, num2, num3) {
+
+}
+
+displayDayTime();
+areaPerimeter(1, 2, 3);
